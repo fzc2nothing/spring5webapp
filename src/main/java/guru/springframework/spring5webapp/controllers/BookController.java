1a16c7df3be558654ddbdd790cdf11f32211f954
rty <45480250+fzc2nothing@users.noreply.github.com>
2020-05-24 18:07:47 -0400

    update

--- src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
+++ src/main/java/guru/springframework/spring5webapp/controllers/BookController.java
@@ -5,11 +5,12 @@ import guru.springframework.spring5webapp.repositories.BookRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class BookController {
 
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
 
     public BookController(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
@@ -20,7 +21,7 @@ public class BookController {
 
         model.addAttribute("books",bookRepository.findAll());
 
-        return "books";
+        return "books/list";
     }
 }
 
